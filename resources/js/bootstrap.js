8ef59aec42f3b70e0ced0379a915789c26ddf6ee
Afam <afamka2007@gmail.com>
2026-06-17 11:56:57 +0300

    bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,17 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = '/';
+axios.defaults.baseURL =
+    import.meta.env.VITE_API_URL
+
+axios.defaults.headers.common.Accept =
+    'application/json'
 
 axios.interceptors.request.use(config => {
     const token = localStorage.getItem('token')
 
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`
+        config.headers.Authorization =
+            `Bearer ${token}`
     }
 
     return config
