d8d4408d50e97e3b3e491338014b1b3095b5de20
Afam <afamka2007@gmail.com>
2026-06-18 02:33:26 +0300

    исправленный .env.example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Парсер организаций</title>
     @vite(['resources/js/app.js'])
 </head>
 <body>
