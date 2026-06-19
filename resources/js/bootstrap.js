0696e160420c4c1902064525394cc619c32cc2af
Afam <afamka2007@gmail.com>
2026-06-19 06:52:01 +0300

    решение проблемы с CORS

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-axios.defaults.baseURL =
-    import.meta.env.VITE_API_URL
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
-axios.defaults.headers.common.Accept =
-    'application/json'
+axios.defaults.headers.common.Accept = 'application/json'
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 axios.interceptors.request.use(config => {
     const token = localStorage.getItem('token')
