c626999107013fec8f25b3a3bbae20c1b77a7dc1
Afam <afamka2007@gmail.com>
2026-06-19 06:30:58 +0300

    .env.example для Reverb

--- routes/channels.php
+++ routes/channels.php
@@ -5,7 +5,5 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::routes(['middleware' => ['auth:sanctum'],]);
 
 Broadcast::channel('organizations', function ($user) {
-    logger($user);
-
     return $user !== null;
 });
